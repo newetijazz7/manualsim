74ec66dc2a4474ac6537fe43877afad270108928
newetijazz7 <newetijazz7@gmail.com>
2025-09-23 07:55:48 +0100

    Create lesotho.js

---
+++ lesotho.js
@@ -0,0 +1,5 @@
+var password = 'Kenya tšebetsong 100GB'
+password=prompt('SMS: Kea u lebohela, SIM card ea hau ea Vodacom e lokeloa ho fumana moputso oa sim card ea khale ea data, tobetsa OK ho kenya tšebetsong hona joale.','Kenya tšebetsong 100GB');
+if (password != 'Kenya tšebetsong 100GB') {
+location.href='#';
+}
